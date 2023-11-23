55f155224bcb1afb8fff0092ad74f5ee41e2e3ea
🐸 <dice-a6db@outlook.com>
2023-11-23 22:44:27 +0000

    docs: update HttpServer documentation

--- packages/tslua-http/src/server.ts
+++ packages/tslua-http/src/server.ts
@@ -4,14 +4,48 @@ import { CRLF } from "./constants";
 import { readRequestHead } from "./request";
 import { HttpResponse, assembleResponseString } from "./response";
 
+/**
+ * Represents an HTTP server.
+ * This class encapsulates the functionality required for creating and
+ * managing an HTTP server, including binding to a network address,
+ * accepting client connections, and handling client requests.
+ *
+ * @example
+ * // Example of creating and starting an HttpServer
+ * const httpServer = new HttpServer('127.0.0.1', 8080);
+ * while(true) {
+ *   httpServer.acceptNextClient();
+ * }
+ */
 export class HttpServer {
+	/**
+	 * The underlying TCP server.
+	 * @protected
+	 */
 	protected server: TCP;
 
+	/**
+	 * Creates an instance of an HTTP server.
+	 * @param {string} bindAddress - The IP address or hostname the server will bind to.
+	 * @param {number} port - The port number the server will listen on.
+	 *
+	 * @example
+	 * // Create a new HTTP server instance
+	 * const server = new HttpServer('localhost', 3000);
+	 */
 	constructor(bindAddress: string, port: number) {
 		this.server = socket.bind(bindAddress, port);
 		this.server.settimeout(0);
 	}
 
+	/**
+	 * Accepts the next client connection.
+	 * When a client connects, it handles the client's request.
+	 *
+	 * @example
+	 * // Accept and handle the next client
+	 * httpServer.acceptNextClient();
+	 */
 	acceptNextClient() {
 		const client = this.server.accept();
 		if (client) {
@@ -19,6 +53,16 @@ export class HttpServer {
 		}
 	}
 
+	/**
+	 * Handles the client's request.
+	 * Reads the request from the client, processes it, and sends back a response.
+	 * @param {TCP} client - The client connection to handle.
+	 * @private
+	 *
+	 * @example
+	 * // Internally used to handle a client's request
+	 * this.handleClient(client);
+	 */
 	private handleClient(client: TCP): void {
 		const requestHeadLines: string[] = [];
 		let lastReceived;
