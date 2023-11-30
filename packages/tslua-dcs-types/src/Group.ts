079c3dbaaf7746d8a379fff163538aee743bc8f2
🐸 <dice-a6db@outlook.com>
2023-11-30 23:23:34 +0000

    feat(tslua-dcs-types): add group.getSize and getUnits

--- packages/tslua-dcs-types/src/Group.ts
+++ packages/tslua-dcs-types/src/Group.ts
@@ -1,3 +1,4 @@
+import { IUnit } from "./Unit";
 import { _Group } from "./exports/Group.export";
 
 export interface IGroup extends _Group {
@@ -26,4 +27,28 @@ export interface IGroup extends _Group {
 	 * @see https://wiki.hoggitworld.com/view/DCS_func_getCategory
 	 */
 	getCategory(): number;
+
+	/**
+	 * Returns the current size of the group. This value will change as units are destroyed.
+	 * Can be used in combination with getUnit to not accidentally use to big a value for that function, or to access the last unit in the group.
+	 *
+	 * @see https://wiki.hoggitworld.com/view/DCS_func_getSize
+	 */
+	getSize(): number;
+
+	/**
+	 * Returns a table of unit objects indexed in unit order.
+	 * Useful for getting unit specific data for every unit in the group.
+	 *
+	 * @see https://wiki.hoggitworld.com/view/DCS_func_getUnits
+	 */
+	getUnits(): IUnit[];
+
+	/**
+	 * Returns the unit object of the specified unitIndex within the group.
+	 * If the index is not valid, this function will return nil.
+	 *
+	 * @param index
+	 */
+	getUnit(index: number): IUnit | undefined;
 }
