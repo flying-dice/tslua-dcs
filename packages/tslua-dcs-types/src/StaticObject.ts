4499f80c8739f8e40ff670265a52a6bca0947e20
🐸 <dice-a6db@outlook.com>
2023-12-11 23:28:35 +0000

    feat(tslua-dcs): add static object definitions

--- packages/tslua-dcs-types/src/StaticObject.ts
+++ packages/tslua-dcs-types/src/StaticObject.ts
@@ -1,5 +1,18 @@
+import { l_Position3, l_Vec3 } from "./coord";
 import { _StaticObject } from "./exports/StaticObject.export";
 export interface l_StaticObject extends _StaticObject {
+	/**
+	 * Returns a number which defines the unique mission id of a given object.
+	 */
+	getID(): number;
+
+	/**
+	 * Returns the name of the group.
+	 *
+	 * @see https://wiki.hoggitworld.com/view/DCS_func_getName
+	 */
+	getName(): string;
+
 	/**
 	 * Returns an instance of the calling class for the object of a specified name.
 	 * The objects name is defined either in the mission editor or within functions that can dynamically spawn objects.
@@ -20,4 +33,36 @@ export interface l_StaticObject extends _StaticObject {
 	 * Returns an enumerator that defines the country that an object currently belongs to
 	 */
 	getCountry(): number;
+
+	/**
+	 * Returns a vec3 table of the x, y, and z coordinates for the position of the given object in 3D space.
+	 * Coordinates are dependent on the position of the maps origin.
+	 *
+	 * In the case of the Caucuses theater, the origin is located in the Crimean region of the map.
+	 *
+	 * @see https://wiki.hoggitworld.com/view/DCS_func_getPoint
+	 */
+	getPoint(): l_Vec3;
+
+	/**
+	 * Returns a Position3 table of the objects current position and orientation in 3D space.
+	 * X, Y, Z values are unit vectors defining the objects orientation.
+	 * Coordinates are dependent on the position of the maps origin.
+	 * In the case of the Caucuses theater, the origin is located in the Crimean region of the map.
+	 *
+	 * @see https://wiki.hoggitworld.com/view/DCS_func_getPosition
+	 */
+	getPosition(): l_Position3;
+
+	/**
+	 * Returns an enumerator that defines the country that an object currently belongs to
+	 */
+	getCountry(): number;
+
+	/**
+	 * Returns the coalition of the Static Object.
+	 *
+	 * @see https://wiki.hoggitworld.com/view/DCS_func_getCoalition
+	 */
+	getCoalition(): number;
 }
