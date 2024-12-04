a21098cefed41aeb3fcdd055ed5ffbdaf3d36b28
🎲 <dice-a6db@outlook.com>
2024-12-04 00:00:07 +0000

    feat(tslua-dcs-mission-types): add LLtoMGRS and MGRStoLL function signatures

--- packages/tslua-dcs-mission-types/src/coord.ts
+++ packages/tslua-dcs-mission-types/src/coord.ts
@@ -22,6 +22,51 @@ export interface l_coord extends _coord {
 		lon: number,
 		alt: number,
 	): { x: number; y: number; z: number };
+
+	/**
+	 * Returns the MGRS point of a given latitude and longitude
+	 *
+	 * @see https://wiki.hoggitworld.com/view/DCS_func_LLtoMGRS
+	 *
+	 * @param lat
+	 * @param lon
+	 */
+	LLtoMGRS(lat: number, lon: number): l_MGRS
+
+
+	/**
+	 * Returns multiple values of a given MGRS point in latitude, longitude, and altitude
+	 *
+	 * @see https://wiki.hoggitworld.com/view/DCS_func_MGRStoLL
+	 *
+	 * @param mgrs
+	 */
+	MGRStoLL(mgrs: l_MGRS): LuaMultiReturn<[number, number, number]>;
+}
+
+/**
+ * Represents an MGRS (Military Grid Reference System) point.
+ *
+ * - 4Q ......................GZD only, precision level 6° × 8° (in most cases)
+ * - 4Q FJ ...................GZD and 100 km Grid Square ID, precision level 100 km
+ * - 4Q FJ 1 6 ...............precision level 10 km
+ * - 4Q FJ 12 67 .............precision level 1 km
+ * - 4Q FJ 123 678 ...........precision level 100 m
+ * - 4Q FJ 1234 6789 .........precision level 10 m
+ * - 4Q FJ 12345 67890 .......precision level 1 m
+ *
+ * @see https://www.digitalcombatsimulator.com/en/support/faq/1256/
+ * @noSelf
+ */
+export interface l_MGRS {
+	/** Easting value of the MGRS point. */
+	Easting: number;
+	/** MGRS digraph (grid square identifier). */
+	MGRSDigraph: string;
+	/** Northing value of the MGRS point. */
+	Northing: number;
+	/** UTM zone of the MGRS point. */
+	UTMZone: string;
 }
 
 /**
